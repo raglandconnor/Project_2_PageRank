fc112adb358a821326af86dab568bda3c7df5e93
raglandconnor <raglandconnor@ufl.edu>
2024-03-27 16:41:31 -0400

    Created insertEdge function
    Created a structure using a vector that contains Webpage objects that store hte rank, outPages, and inPages of a particular website.

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -4,39 +4,54 @@
 
 #include "Graph.h"
 
-int Graph::numVertices() {
+//int Graph::numVertices() {
+//
+//}
+//
+//int Graph::numEdges() {
+//
+//}
+//
+//Graph::Graph() {
+//
+//}
+//
+//Graph::Graph(int vertexCount) {
+//
+//}
+//
+//Graph::~Graph() {
+//
+//}
 
+void Graph::insertEdge(std::string from, std::string to) {
+    if (mapper.find(from) == mapper.end()) {
+        mapper[from] = vertexCount++;
+    }
+    if (mapper.find(to) == mapper.end()) {
+        mapper[to] = vertexCount++;
+    }
+
+    while (adjacencyList.size() < vertexCount) {  // Newly mapped objects are being added
+        Webpage webpageObject;
+        webpageObject.rank = (double) 0;
+        adjacencyList.push_back(webpageObject);
+    }
+
+    adjacencyList[mapper[from]].outPages.insert(to);
+    adjacencyList[mapper[to]].inPages.insert(from);
 }
 
-int Graph::numEdges() {
 
-}
+//void Graph::removeEdge(std::string from, std::string to) {
+//
+//}
+//
+//bool Graph::isEdge(std::string from, std::string to) {
+//
+//}
 
-Graph::Graph() {
-
-}
-
-Graph::Graph(int vertexCount) {
-
-}
-
-Graph::~Graph() {
-
-}
-
-void Graph::insertEdge(std::string from, std::string to, int weight) {
-
-}
-
-void Graph::removeEdge(std::string from, std::string to) {
-
-}
-
-bool Graph::isEdge(std::string from, std::string to) {
-
-}
-
-int Graph::getWeight(std::string from, std::string to) {
-
+int Graph::getRank(std::string url) {
+    adjacencyList[mapper[url]]
 }
 
--- src/Graph.h
+++ src/Graph.h
@@ -8,29 +8,39 @@
 
 #include <string>
 #include <map>
+#include <set>
 using namespace std;
+
+
 
 class Graph {
 private:
-    map<string, int> keys;
-    vector<pair<string, int>> adjacencyList;
+    struct Webpage {
+        double rank;
+        set<string> outPages;
+        set<string> inPages;
+    };
 
-    int vertexCount;  // Stores how many vertices are in the graph
+    map<string, int> mapper;
+    vector<Webpage> adjacencyList;
+
+    int vertexCount = 0;  // Stores how many unique vertices are in the graph
 
     vector<string> getAdjacent(string vertex);
 
     int numVertices();  // Returns number of vertices
     int numEdges();  // Returns number of edges
 
+
 public:
     Graph();
     Graph(int vertexCount);  // Creates graph with V vertices
     ~Graph();  // Destructor
 
-    void insertEdge(string from, string to, int weight);
+    void insertEdge(string from, string to);
     void removeEdge(string from, string to);
     bool isEdge(string from, string to);
-    int getWeight(string from, string to);
+    int getRank(string url);
 };
 
 
